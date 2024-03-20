1c47187a1f4ab46aae8bfb3c2ba73f9929ee48c0
PeterMoschitto <pdmoschitto@gmail.com>
2024-03-20 09:15:13 -0700

    Finalize form project

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -6,13 +6,22 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/p1")
+@app.route("/results")
 def render_page1():
-    return render_template('page1.html')
+    
+    
+	name = request.args['name']
+	height = request.args['height']
+	weight = request.args['weight']
+	deadlift = request.args['deadlift']
+    
+	ratio = (int(height) + int(weight)) / 3
+	deadliftRatio = int(deadlift) / 70
+	velo1 = ratio + deadliftRatio
+	velo2 = round(velo1)
+	
+	return render_template('page1.html', response1 = name, response2 = height, response3 = weight, response4 = deadlift, velo = velo2)
 
-@app.route("/p2")
-def render_page2():
-    return render_template('page2.html')
     
 if __name__=="__main__":
     app.run(debug=False)
